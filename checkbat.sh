71415987022e07237e1f56ca27f2579b140308d4
Clare Donnelly <namingFailed@gmail.com>
2013-04-28 14:47:09 +0100

    fixed checkbat.sh so it only gives notification on a low battery (less than an hour remaining). Cron line is also included for reference

--- checkbat.sh
+++ checkbat.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+#every minute where battery is less than 1hr charge remaining give warning
+#Requires: acpi, libnotify
+#Cron:
+# */1 * * * *  env DISPLAY=:0 /home/clare/projects/configstuff/checkbat.sh
+  
 bat=`acpi -b`
-notify-send "$bat"
+hours=$(echo $bat | cut -f 5 -d " " | cut -f 1 -d ":")
+charging=`acpi -b | cut -f 3 -d " "`
+if [[ $charging != "Charging," ]]
+then
+    if [ $hours -lt 1 ]
+        then
+            notify-send "WARNING: $bat"
+    fi
+fi
+
