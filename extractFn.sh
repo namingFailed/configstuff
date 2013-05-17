69ccd95abf83b018df9b3d45d1b74ce3d71e3be0
Clare Donnelly <namingFailed@gmail.com>
2013-05-17 09:36:40 +0100

    commiting to switch branch

--- extractFn.sh
+++ extractFn.sh
@@ -14,10 +14,13 @@ then
     exit 3
 fi
 filetype=`exec ruby test.rb $1`
-if [[ $filetype != "Shell" ]]
+if [[ $filetype != "Shell" ]] 
 then
-    echo "I'm sorry only Shell script supported right now :(" 1>&2
+    if [[ $filetype != "Python" ]]
+    then
+    echo "I'm sorry only Shell and Python script supported right now :(" 1>&2
     exit 4
+    fi
 fi
 fname=$2
 file=$1
@@ -50,9 +53,53 @@ function forShell() {
         
     done < <(cat $file)
 }
+function forPy() {
+    kword="def"
+    kwordl=`expr ${#kword} + 1`
+    endlen=`expr ${#kwordl} + ${#fname} - 1 `
+    indentLevel=0
+    #need to find the white space
+    IFS=""
+    while read line;
+    do
+        t=${line:$kwordl:$endlen}
+        if [[ $t == "$fname" ]]
+        then
+            infun=true
+            delay=0
+            havefun=true
+            #if this is a string then indentlevel =1 etc
+            echo `echo $line | awk '/ / {print $1}'`
+            indentLevel=`echo $line | tr -dc " " | wc -c`
+            echo $line
+        fi
+        if $infun;
+        then
+            if [[ $delay -ge 1 ]]
+            then
+            n=`echo $line | tr -dc " " | wc -c` 
+            if [[ `echo $line | tr -dc " " | wc -c` == $indentLevel ]]
+            then
+                echo $line
+                infun=false
+            else
+                echo $line
+            fi
+            fi
+        fi
+        if [[ $delay == 0 ]]
+        then
+            delay=1
+        fi
+    done < <(cat $file)
+}
 if [[ $filetype == "Shell" ]]
 then
     forShell
+fi
+if [[ $filetype == "Python" ]]
+then
+    forPy
 fi
 if ! $havefun;
 then
